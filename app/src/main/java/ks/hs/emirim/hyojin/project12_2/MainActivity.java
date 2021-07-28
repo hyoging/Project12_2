74ef65740e676a8426558bede6a96d254accb35a
User <w2030@e-mirim.hs.kr>
2021-07-28 14:32:30 +0900

    Edit MainActivity(update,delete) Commit

--- app/src/main/java/ks/hs/emirim/hyojin/project12_2/MainActivity.java
+++ app/src/main/java/ks/hs/emirim/hyojin/project12_2/MainActivity.java
@@ -14,22 +14,25 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     MyDBHelper dbHelper;
-    EditText editName, editCount, editResult, editNameResult, editCountResult;
+    EditText editName, editCount, editNameResult, editCountResult;
     SQLiteDatabase db;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         editName = findViewById(R.id.edit_name);
         editCount = findViewById(R.id.edit_count);
-        editResult = findViewById(R.id.edit_count);
         editNameResult = findViewById(R.id.edit_name_result);
         editCountResult = findViewById(R.id.edit_count_result);
         Button btnInit = findViewById(R.id.btn_init);
         Button btnInput = findViewById(R.id.btn_input);
-        Button btnsearch = findViewById(R.id.btn_search);
+        Button btnUpdate = findViewById(R.id.btn_update);
+        Button btnDelete = findViewById(R.id.btn_delete);
+        Button btnSearch = findViewById(R.id.btn_search);
+
+        dbHelper = new MyDBHelper(this);
+
+
 
         dbHelper = new MyDBHelper(this);
         btnInit.setOnClickListener(new View.OnClickListener() {
@@ -49,7 +52,25 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"정상적으로 행이 삽입되었습니다.", Toast.LENGTH_SHORT).show();
             }
         });
-        btnsearch.setOnClickListener(new View.OnClickListener() {
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db = dbHelper.getWritableDatabase();
+                db.execSQL("update groupTB set count = " + editCount.getText().toString() + " where name = '" + editName.getText().toString() + "';");
+                selectDB();
+                db.close();
+            }
+        });
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db = dbHelper.getWritableDatabase();
+                db.execSQL("delete from groupTB where name = '" + editName.getText().toString() + "';");
+                selectDB();
+                db.close();
+            }
+        });
+        btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 db = dbHelper.getReadableDatabase();
@@ -69,6 +90,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void selectDB(){
+        db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from groupTB;", null);
+        String strName = "그룹 이름\r\n_________\r\n";
+        String strCount = "인원\r\n_________\r\n";
+        while(cursor.moveToNext()){
+            strName += cursor.getString(0) + "\r\n";
+            strCount += cursor.getInt(1) + "\r\n";
+        }
+        editNameResult.setText(strName);
+        editCountResult.setText(strCount);
+
+        cursor.close();
+        db.close();
+    }
     public class MyDBHelper extends SQLiteOpenHelper{
 
         public MyDBHelper(Context context){
@@ -76,12 +113,11 @@ public class MainActivity extends AppCompatActivity {
         }
         @Override
         public void onCreate(SQLiteDatabase db) {
-            db.execSQL("create table groupTB(name char(20) primary key, count integer);");
+            db.execSQL("CREATE TABLE groupTB (name CHAR(20) PRIMARY KEY, count INTEGER);");
         }
-
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("drop table if exists groupTB ");
+            db.execSQL("drop table if exists groupTB");
             onCreate(db);
         }
     }
